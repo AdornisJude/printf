2e6ef433cbc70152357a1d4097a8a08478e3c4c6
AdornisJude <Jude_Adornis@yahoo.com>
2023-08-19 13:16:49 -0700

    Write a function that produces output according to a format. Prototype: int _printf(const char *format, ...); Returns: the number of characters printed (excluding the null byte used to end output to strings), write output to stdout, the standard output stream format is a character string. The format string is composed of zero or more directives. See man 3 printf for more detail. You need to handle the following conversion specifiers: (c, s, %)

--- _printf.c
+++ _printf.c
@@ -3,74 +3,87 @@
 #include <unistd.h>
 
 /**
-* _printf - prints to stdout
-* @format: format specifier
-* Return: total count of characters printed
-*/
-
-// Custom putchar function to write a character to standard output
+ * custom_putchar - writes a single character to stdout
+ * @c: the character to print
+ *
+ * Return: the number of characters written
+ */
 int custom_putchar(char c)
 {
-    return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
 
-// Custom function to handle printing strings
+/**
+ * custom_string - handles printing of strings
+ * @str: the string to print
+ *
+ * Return: the number of characters printed
+ */
 int custom_string(char *str)
 {
-    int i = 0;
-    int count = 0;
-    if (str == NULL)
-    {
-        custom_string("(null)");
-        return (i);
-    }
-    while (str[i])
-    {
-        custom_putchar(str[i]);
-        i++;
-    }
-    return (i);
+	int i = 0;
+	int count = 0;
+
+	if (str == NULL)
+	{
+		custom_string("(null)");
+		return (i);
+	}
+	while (str[i])
+	{
+		custom_putchar(str[i]);
+		i++;
+	}
+	return (i);
 }
 
-// Custom printf implementation
+/**
+ * custom_printf - custom implementation of printf
+ * @format: format specifier
+ * @...: variable arguments
+ *
+ * Return: total count of characters printed
+ */
 int custom_printf(const char *format, ...)
 {
-    int count = 0;
+	int count = 0;
+	va_list func_arg;
 
-    va_list func_arg;
-    va_start(func_arg, format);
+	va_start(func_arg, format);
 
-    if (format != NULL || !format[0])
-        return (-1);
+	if (format == NULL || !format[0])
+		return (-1);
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (*format == 'c')
-            {
-                char c = va_arg(func_arg, int);
-                count = count + custom_putchar(c);
-            }
-            else if (*format == 's')
-            {
-                char *str = va_arg(func_arg, char *);
-                count = custom_string(str);
-            }
-            else if (*format == '%')
-            {
-                custom_putchar('%');
-                count++;
-            }
-            else
-            {
-                custom_putchar(*format);
-                count++;
-            }
-        }
-        format++;
-    }
-    va_end(func_arg);
-    return (count);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+			{
+				char c = va_arg(func_arg, int);
+
+				count = count + custom_putchar(c);
+			}
+			else if (*format == 's')
+			{
+				char *str = va_arg(func_arg, char *);
+
+				count = custom_string(str);
+			}
+			else if (*format == '%')
+			{
+				custom_putchar('%');
+				count++;
+			}
+			else
+			{
+				custom_putchar(*format);
+				count++;
+			}
+		}
+		format++;
+	}
+	va_end(func_arg);
+	return (count);
 }
