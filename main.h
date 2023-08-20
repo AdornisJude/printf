fcee2f4c9708ce2a90a32ff2aca1be6e367c8e3d
AdornisJude <Jude_Adornis@yahoo.com>
2023-08-20 11:59:02 -0700

    Write a function that produces output according to a format. Prototype: int _printf(const char *format, ...); Returns: the number of characters printed (excluding the null byte used to end output to strings), write output to stdout, the standard output stream format is a character string. The format string is composed of zero or more directives. See man 3 printf for more detail. You need to handle the following conversion specifiers: (c, s, %)

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+int _string(char *str);
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+#endif
