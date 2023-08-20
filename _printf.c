cccc4feba8386a98fd5d046dbddc48e32f0aa9ab
AdornisJude <Jude_Adornis@yahoo.com>
2023-08-20 11:58:00 -0700

    Write a function that produces output according to a format. Prototype: int _printf(const char *format, ...); Returns: the number of characters printed (excluding the null byte used to end output to strings), write output to stdout, the standard output stream format is a character string. The format string is composed of zero or more directives. See man 3 printf for more detail. You need to handle the following conversion specifiers: (c, s, %)

--- _printf.c
+++ _printf.c
@@ -1,50 +1,13 @@
-#include <stdarg.h>
-#include <stdio.h>
-#include <unistd.h>
+#include "main.h"
 
 /**
- * custom_putchar - writes a single character to stdout
- * @c: the character to print
- *
- * Return: the number of characters written
- */
-int custom_putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * custom_string - handles printing of strings
- * @str: the string to print
- *
- * Return: the number of characters printed
- */
-int custom_string(char *str)
-{
-	int i = 0;
-	int count = 0;
-
-	if (str == NULL)
-	{
-		custom_string("(null)");
-		return (i);
-	}
-	while (str[i])
-	{
-		custom_putchar(str[i]);
-		i++;
-	}
-	return (i);
-}
-
-/**
- * custom_printf - custom implementation of printf
+ * _printf - custom implementation of printf
  * @format: format specifier
  * @...: variable arguments
  *
  * Return: total count of characters printed
  */
-int custom_printf(const char *format, ...)
+int _printf(const char *format, ...)
 {
 	int count = 0;
 	va_list func_arg;
@@ -54,33 +17,39 @@ int custom_printf(const char *format, ...)
 	if (format == NULL || !format[0])
 		return (-1);
 
-	while (*format)
+	while (*format != '\0')
 	{
 		if (*format == '%')
-		{
+		{	
 			format++;
+
 			if (*format == 'c')
 			{
 				char c = va_arg(func_arg, int);
-
-				count = count + custom_putchar(c);
-			}
-			else if (*format == 's')
-			{
-				char *str = va_arg(func_arg, char *);
-
-				count = custom_string(str);
-			}
-			else if (*format == '%')
-			{
-				custom_putchar('%');
+				_putchar(c);
 				count++;
 			}
-			else
+			else if (*format =='s')
 			{
-				custom_putchar(*format);
-				count++;
+				char(*str) = va_arg(func_arg, char *);
+
+				while (*str != '\0')
+				{
+					_putchar(*str);
+					str++;
+					count++;
+				}	
 			}
+		}
+		else if (*format == '%')
+		{
+			_putchar('%');
+			count++;
+		}
+		else
+		{
+			_putchar(*format);
+			count++;
 		}
 		format++;
 	}
